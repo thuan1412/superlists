95a656b3a5c560e66afad7d271d426e183ad7758
thuan.dh <thuan14121999@gmail.com>
2019-11-13 22:04:28 +0700

    refactor home page view to use a template

---
+++ lists/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_template(self):
+        response = self.client.get('/')
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
+
+    # def test_home_page_returns_correct_html(self):
+    #     response = self.client.get('/')
+    #     html = response.content.decode('utf-8')
+    #     expected_html = render_to_string('home.html')
+    #     self.assertEqual(html, expected_html)
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lists import views
+from django.conf.urls import url
 
 urlpatterns = [
+    url(r'^$', views.home_page, name='home'),
     path('admin/', admin.site.urls),
 ]
