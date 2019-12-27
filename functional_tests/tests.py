087b22a863fe1f9f72ebc9bb02b21b859b077532
thuan.dh <thuan14121999@gmail.com>
2019-12-27 23:09:34 +0700

    change explicit wait to implicit on funtional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,20 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.common.exceptions import WebDriverException
+
 from django.test import LiveServerTestCase
 
+import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
     def tearown(self):
+        print("Not exit")
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+        # self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -27,15 +42,14 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item')
 
-        inputbox.send_keys('Buy peacock feathres to make a fly')
+        inputbox.send_keys('Buy peacock feathres')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
-        self.check_for_row_in_list_table('Buy peacock feathres')
 
-        # inputbox = self.browser.find_element_by_id('id_new_item')
-        # inputbox.send_keys('Use peacock feathers to make a fly')
-        # inputbox.send_keys(Keys.ENTER)
-        # time.sleep(1)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-        self.check_for_row_in_list_table('Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathres')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
         self.fail('Finish the test!')
